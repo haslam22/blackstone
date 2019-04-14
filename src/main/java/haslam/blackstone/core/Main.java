d1e830b10c03026f01ce042b8cd16d416e7d41c0
Hasan Aslam <haslam22@users.noreply.github.com>
2019-04-14 17:08:35 +0100

    Add Java 12 support.

---
+++ src/main/java/haslam/blackstone/core/Main.java
@@ -0,0 +1,12 @@
+package haslam.blackstone.core;
+
+/**
+ * Required due to JavaFX weirdness in Java 11 +.
+ * http://mail.openjdk.java.net/pipermail/openjfx-dev/2018-June/021977.html
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Launcher.main(args);
+    }
+}
